340bbf6dd50d73a958e6b5c9e3e24bd041dc3fb5
kentwberry4 <kentwberry@protonmail.com>
2023-01-25 21:56:48 -0500

    First commit, finishing up and old project

---
+++ src/main/java/guru/springframework/models/Difficulty.java
@@ -0,0 +1,5 @@
+package guru.springframework.models;
+
+public enum Difficulty {
+    EASY, MODERATE, KIND_OF_HARD, HARD
+}
---
+++ src/main/java/guru/springframework/repos/UnitOfMeasureRepository.java
@@ -0,0 +1,10 @@
+package guru.springframework.repos;
+
+import guru.springframework.models.UnitOfMeasure;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+    Optional<UnitOfMeasure> findByDescription(String description);
+}
