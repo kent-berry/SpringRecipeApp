e34b3cb13ed700cbcecbe55ed39cb5ead24b84d8
kentwberry4 <kentwberry@protonmail.com>
2023-02-11 13:21:58 -0500

    added config for mysql

--- src/main/resources/scripts/mysql-config.sql
+++ src/main/resources/scripts/mysql-config.sql
@@ -1,1 +1,15 @@
-CREATE
+CREATE DATABASE recipe_dev;
+CREATE DATABASE recipe_prod;
+
+CREATE USER 'recipe_dev_user'@'localhost' IDENTIFIED BY 'berry';
+CREATE USER 'recipe_prod_user'@'localhost' IDENTIFIED BY 'berry';
+
+GRANT SELECT ON recipe_dev.* to 'recipe_dev_user'@'localhost';
+GRANT INSERT ON recipe_dev.* to 'recipe_dev_user'@'localhost';
+GRANT DELETE ON recipe_dev.* to 'recipe_dev_user'@'localhost';
+GRANT UPDATE ON recipe_dev.* to 'recipe_dev_user'@'localhost';
+
+GRANT SELECT ON recipe_prod.* to 'recipe_prod_user'@'localhost';
+GRANT INSERT ON recipe_prod.* to 'recipe_prod_user'@'localhost';
+GRANT DELETE ON recipe_prod.* to 'recipe_prod_user'@'localhost';
+GRANT UPDATE ON recipe_prod.* to 'recipe_prod_user'@'localhost';
